5f3225e49b44fa3efd88c15c8247b6f11880fae0
Namh141255 <namh7043@gmail.com>
2024-04-04 12:08:39 +0700

    add complete update admin password function

--- app/Http/Controllers/Admin/AdminController.php
+++ app/Http/Controllers/Admin/AdminController.php
@@ -4,7 +4,9 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Admin;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
 use Validator;
 
 class AdminController extends Controller
@@ -44,5 +46,34 @@ class AdminController extends Controller
     public function logout(){
         Auth::guard('admin')->logout();
         return redirect('admin/login');
+    }
+
+    public function updatePassword(Request $request){
+        if($request->isMethod('post')){
+            $data = $request->all();
+            // Check if current password is correct 
+            if(Hash::check($data['current_pwd'],Auth::guard('admin')->user()->password)){
+                //Check if new password and confirm password are matching
+                if($data['new_pwd']==$data['confirm_pwd']){
+                    //Update new password
+                    Admin::where('id',Auth::guard('admin')->user()->id)->update(['password'=> bcrypt($data['new_pwd'])]);
+                    return redirect()->back()->with('success_message','Password has been updated Successfully!');
+                }else{
+                    return redirect()->back()->with('error_message','New password and Retype password not match!');
+                }
+            }else{
+                return redirect()->back()->with('error_message','Your current password is Incorrect!');
+            }
+        }
+        return view('admin.update_password');
+    }
+
+    public function checkCurrentPassword(Request $request){
+        $data = $request->all();
+        if(Hash::check($data['current_pwd'],Auth::guard('admin')->user()->password)){
+            return "true";
+        }else{
+            return "false";
+        }
     }
 }
